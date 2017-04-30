27b7e168ac6aa3d26cf2f91fd4e86698f98aca2f
Guilherme Andrade <g@gandrade.net>
2017-04-30 16:34:49 +0100

    Simplify building process

--- scripts/hackish_make_docs.sh
+++ scripts/hackish_make_docs.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 # sigh.....
-sed -i -e 's/^\(\s*\)%{edown/\1{edown/g' -e 's/^\(\s*\)%{doclet, edown/\1{doclet, edown/g' rebar.config
-rebar3 compile
-mkdir -p _build/default//lib/erlffx/doc/
-cp -p overview.edoc _build/default/lib/erlffx/doc/
-erl -pa _build/default/lib/*/ebin -noshell -run edoc_run application "erlffx"
-erl -pa _build/default/lib/*/ebin -noshell -run edoc_run application "erlffx" '[{doclet, edown_doclet}, {top_level_readme, {"README.md", "https://github.com/g-andrade/erlffx", "master"}}]'
+rebar3 as generate_documentation compile
+mkdir -p _build/generate_documentation/lib/erlffx/doc/
+cp -p overview.edoc _build/generate_documentation/lib/erlffx/doc/
+erl -pa _build/generate_documentation/lib/*/ebin -noshell -run edoc_run application "erlffx"
+erl -pa _build/generate_documentation/lib/*/ebin -noshell -run edoc_run application "erlffx" '[{doclet, edown_doclet}, {top_level_readme, {"README.md", "https://github.com/g-andrade/erlffx", "master"}}]'
 rm -rf doc
-mv _build/default/lib/erlffx/doc ./
-sed -i -e 's/^\(\s*\){edown/\1%{edown/g' -e 's/^\(\s*\){doclet, edown/\1%{doclet, edown/g' rebar.config
+mv _build/generate_documentation/lib/erlffx/doc ./
 sed -i -e 's/^\(---------\)$/\n\1/g' README.md
